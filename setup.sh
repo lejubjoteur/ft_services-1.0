7bf82b06087853ddbe909af18a1fe43f05db1f05
Quentin GIMENEZ <qgimenez@e2r3p12.42.fr>
2020-01-29 15:32:38 +0100

    29/01

--- setup.sh
+++ setup.sh
@@ -2,7 +2,9 @@ minikube addons enable ingress
 minikube addons enable metrics-server
 eval $(minikube docker-env)
 docker build -t mynginx srcs/nginx/
+docker build -t myftps srcs/ftps/
 docker build -t my-grafana srcs/grafana
 docker build -t my-phpmyadmin:1.1 srcs/phpmyadmin
 docker build -t my-wordpress:1.5 srcs/wordpress/
 kubectl apply -k srcs
+kubectl get svc
